aac07b2b3dd88507cd4a1c9481856aeb0f9c81da
AlessioRomano95 <romanalex400@gmail.com>
2024-02-20 14:56:58 +0100

    [ADD] colore , numero  e somme

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ const firstname = prompt('Nome?')
 
 const lastname = prompt('Cognome')
 
+const color = prompt('Colore preferito?')
+
+const fixedNumber = 24
+
+let message = 'La tua nuova password è: '
+
+const finalText = '<strong>' + message + '</strong>' + firstname + lastname + color + fixedNumber;
+
+element.innerHTML = finalText
